9fb3aa6241dc6fb750411ea03f0cea23cb5df409
Peter Grassberger <petertheone@gmail.com>
2018-05-11 20:45:41 +0200

    show relative times

--- functions.inc.php
+++ functions.inc.php
@@ -16,7 +16,7 @@ function loadTemplate($page, $params, $layout = true) {
 }
 
 // see: https://stackoverflow.com/a/45676345/782920
-function csvToArray ($file_path) {
+function csvToArray($file_path) {
     $file = fopen($file_path, 'r');
     $headers = fgetcsv($file);
 
@@ -30,4 +30,26 @@ function csvToArray ($file_path) {
     }
     fclose($file);
     return $data;
+}
+
+function timestampToPrettyDate($timestamp) {
+    $datetime = (new DateTime())->setTimestamp((int) $timestamp);
+    $interval = (new DateTime())->diff($datetime);
+    if ($datetime > (new DateTime())->sub(new DateInterval('PT1M'))) {
+        $n = $interval->format('%s');
+        return 'vor ' . $n . ($n <= 1 ? ' Sekunde' : ' Sekunden');
+    }
+    if ($datetime > (new DateTime())->sub(new DateInterval('PT1H'))) {
+        $n = $interval->format('%i');
+        return 'vor ' . $n . ($n <= 1 ? ' Minute' : ' Minuten');
+    }
+    if ($datetime > (new DateTime())->sub(new DateInterval('P1D'))) {
+        $n = $interval->format('%h');
+        return 'vor ' . $n . ($n <= 1 ? ' Stunde' : ' Stunden');
+    }
+    if ($datetime > (new DateTime())->sub(new DateInterval('P7D'))) {
+        $n = $interval->format('%d');
+        return 'vor ' . $n . ($n <= 1 ? ' Tag' : ' Tagen');
+    }
+    return date_format((new DateTime())->setTimestamp((int) $timestamp), 'd.m.Y H:i:s');
 }
--- theme/index.php
+++ theme/index.php
@@ -6,7 +6,7 @@
       <article class="article">
         <header>
           <span><?php echo htmlentities($article->site, ENT_QUOTES, 'UTF-8'); ?></span> -
-          <span><?php echo htmlentities(date_format((new DateTime())->setTimestamp($article->created), 'd.m.Y H:i:s'), ENT_QUOTES, 'UTF-8'); ?></span>
+          <span title="<?php echo date_format((new DateTime())->setTimestamp((int) $article->created), 'd.m.Y H:i:s'); ?>"><?php echo timestampToPrettyDate((int) $article->created); ?></span>
         </header>
         <h2><a href="<?php echo htmlentities($article->link, ENT_QUOTES, 'UTF-8'); ?>" target="_blank"><?php echo htmlentities($article->title, ENT_QUOTES, 'UTF-8'); ?></a></h2>
       </article>
