b25e6c46802e432b7b6dd832a78d31c62e6a3762
Peter Grassberger <petertheone@gmail.com>
2018-05-11 17:36:05 +0200

    prevent cross-site scripting

--- theme/index.php
+++ theme/index.php
@@ -4,12 +4,11 @@
   ?>
     <div class="col-lg-4 col-sm-6">
       <article class="article">
-        <!-- todo: check security -->
         <header>
-          <span><?php echo $article->site; ?></span> -
-          <span><?php echo date_format((new DateTime())->setTimestamp($article->created), 'd.m.Y H:i:s'); ?></span>
+          <span><?php echo htmlentities($article->site, ENT_QUOTES, 'UTF-8'); ?></span> -
+          <span><?php echo htmlentities(date_format((new DateTime())->setTimestamp($article->created), 'd.m.Y H:i:s'), ENT_QUOTES, 'UTF-8'); ?></span>
         </header>
-        <h2><a href="<?php echo $article->link; ?>" target="_blank"><?php echo $article->title; ?></a></h2>
+        <h2><a href="<?php echo htmlentities($article->link, ENT_QUOTES, 'UTF-8'); ?>" target="_blank"><?php echo htmlentities($article->title, ENT_QUOTES, 'UTF-8'); ?></a></h2>
       </article>
     </div>
   <?php
